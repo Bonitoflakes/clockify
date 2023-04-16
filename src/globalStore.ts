521f879af8cdc13df174d3679aa64844bc035866
rishabkhivsara <rishabjain3500@gmail.com>
2023-04-16 12:53:28 +0530

    feat(STATE): Add new states

--- src/globalStore.ts
+++ src/globalStore.ts
@@ -1,5 +1,13 @@
 import { createProxy, subscribe, subscribePrimitive } from "../R&D/proxy2";
 
-const Store = createProxy({ isSidebarOpen: true, timer: [0, 0, 0], isTimerStarted: false });
+const Store = createProxy({
+  isSidebarOpen: true,
+  timer: [0, 0, 0],
+  isTimerStarted: false,
+  allProjects: [],
+  activeProject: "",
+  filter: "",
+  meow: [],
+});
 
 export { Store, createProxy, subscribe, subscribePrimitive };
